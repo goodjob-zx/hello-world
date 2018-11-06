bb58508acbe3625f3e0b8d3bc96d8261c7a040e3
ZIXUAN <lzx@debian>
2018-11-06 17:08:53 +0800

    modified:   ms_dic.py More features are added to this application, more words can be searched. In one period, multiple words can be searched and recorded in a local txt file, '!' is needed to terminate the program.

--- ms_dic.py
+++ ms_dic.py
@@ -1,3 +1,8 @@
+# A dictionary based on the searching page of Bing Dic
+# search specific tags in the HTML page and print out the meaning of words.
+# requests, BeautifulSoup
+
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -14,25 +19,44 @@ def parsePage(demo):
     soup = BeautifulSoup(demo, 'html.parser')
     print(soup.find('div', 'hd_prUS').string, end = '    ')
     print(soup.find('div', 'hd_pr').string)
-    if soup.find('span', 'bil'):
-        print(soup.find('span', 'bil').string, end = ' ')
-        print(soup.find('span', 'val').string)
-    print('\nE-E:', end = '')
-    count = 1
-    for i in soup('div', 'df_cr_w'):
-        if i.contents[0].name == 'a':
-            print('\n{}. '.format(count), end = '')
-            count += 1
-            for tag in i.children:
-                print (tag.string, end = '')
+#pure English meaning
+    if soup('div', 'each_seg') == [] or soup('div', 'se_lis') == []:
+        seg = soup.find('div', {'id':'homoid'})
+        for tag in seg('tr','def_row df_div1'):
+            print(tag.find('div', 'pos pos1').string)
+            for exp in tag('div', 'de_li1 de_li3'):
+                print(exp.find('div', 'se_d').string, end = ' ')
+                for i in exp('a'):
+                    print (i.string, end = ' ')
+                print('')
+#Chinese and English meaning
+    for seg in soup('div', 'each_seg'):
+        print(seg.find('div', 'pos').string)
+        for tag in seg('div', 'se_lis'):
+            print(tag.find('div', 'se_d').string, end = ' ')
+            if tag.find('span', 'gra'):
+                print(tag.find('span','gra').string, end = ' ')
+            if tag.find('span', 'comple'):
+                print(tag.find('span', 'comple').string, end = ' ')
+            print(tag.find('span', 'bil').string, end = ' ')
+            print(tag.find('span', 'val').string)
     print('')
-    return
 
 def main():
-    word = input()
-    para = {'q':word, 'go':'Search', 'qs':'ds', 'form':'Z9LH5'}
+    os.chdir('/home/lzx/Study/English')
+    record = open('test.txt', 'a')
     kv = {'user-agent':'Mozilla/5.0'}
-    parsePage(getHTML('https://cn.bing.com/dict/search', para, kv))
+    word = input('Enter the word, ! to quit.\n')
+    while word != '!':
+        para = {'q':word, 'go':'Search', 'qs':'ds', 'form':'Z9LH5'}
+        demo = getHTML('https://cn.bing.com/dict/search', para, kv)
+        try:
+            parsePage(demo)
+            record.write(word + '\n')
+        except:
+            print('error')
+        word = input('Enter the word, ! to quit.\n')
+    record.close()
     return
 main()
 
